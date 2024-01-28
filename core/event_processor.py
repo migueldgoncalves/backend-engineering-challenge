de237051361fccc3d026b0072d496889d3781a89
Miguel Gonçalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2024-01-28 23:25:18 +0000

    Finished Core package - Except for Output Generator thread closes #7

--- core/event_processor.py
+++ core/event_processor.py
@@ -61,24 +61,95 @@ class EventProcessor:
         :param window_size: Requested window size, in minutes. For each minute, the previous window_size minutes will be analysed
         :return:
         """
-        def on_first_event(current_chronology: list[dict[str, Union[datetime, int]]], next_minute: datetime, delivery_time: int):
-            # First minute to add to chronology is the full minute before the creation of the first translation event
-            timestamp_previous_minute: datetime = next_minute - timedelta(minutes=1)  # Ex: 12:33:00 -> 12:32:00, for a first event created at 12:32:05
-
-            minute_info: dict[str, Union[datetime, int]] = {
-                EventProcessor.CHRONOLOGY_DATE_KEY: timestamp_previous_minute,
-                EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY: 0,
-                EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY: 0
-            }
-            current_chronology.insert(0, minute_info)
-
-            # Now the current minute for the incoming event can be added
+        def on_first_event(current_chronology: list[dict[str, Union[datetime, int]]], next_minute: datetime, delivery_time: int, target_queue: queue.Queue):
+            # Inserts the first minute in the chronology, the timestamp of the incoming translation event rounded to the next minute
             minute_info: dict[str, Union[datetime, int]] = {
                 EventProcessor.CHRONOLOGY_DATE_KEY: next_minute,
                 EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY: 1,
                 EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY: delivery_time
             }
             current_chronology.insert(0, minute_info)  # Chronology order is from latest to earliest minute
+
+            # First minute to output is the full minute before the creation of the first translation event - This is an empty minute as it has no translation events
+            timestamp_previous_minute: datetime = next_minute - timedelta(minutes=1)  # Ex: 12:33:00 -> 12:32:00, for a first event created at 12:32:05
+            EventProcessor._notify_new_information(target_queue, datetime_object=timestamp_previous_minute, average_delivery_time=0)  # Can be outputted immediately
+
+        def on_event_for_same_minute(current_chronology: list[dict[str, Union[datetime, int]]], delivery_time: int):
+            # Current minute is stored at index 0 of chronology - Once closed, it will regularly be pushed to higher indexes
+            number_of_events: int = current_chronology[0].get(EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY)
+            total_delivery_time: int = current_chronology[0].get(EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY)
+
+            number_of_events += 1
+            total_delivery_time += delivery_time
+
+            # Update info for current minute with values for incoming translation event
+            current_chronology[0][EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY] = number_of_events
+            current_chronology[0][EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY] = total_delivery_time
+
+            # At this instant, current minute is still open as new events can belong to the same minute as this event. Therefore, there is no new information to notify
+
+        def on_event_for_new_consecutive_minute(current_chronology: list[dict[str, Union[datetime, int]]], next_minute: datetime, delivery_time: int, window: int, target_queue: queue.Queue):
+            # The current minute is now closed, with the arrival of a translation event that occurred in the next minute
+            # The info for the current minute should therefore be outputted
+            current_minute = current_chronology[0].get(EventProcessor.CHRONOLOGY_DATE_KEY)  # The current minute until the incoming event arrived
+            average_delivery_time: float = EventProcessor._get_average_delivery_time_from_chronology(current_chronology, window)
+            EventProcessor._notify_new_information(target_queue, datetime_object=current_minute, average_delivery_time=average_delivery_time)
+
+            # The incoming translation event should open a new minute in the chronology
+            minute_info: dict[str, Union[datetime, int]] = {
+                EventProcessor.CHRONOLOGY_DATE_KEY: next_minute,
+                EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY: 1,
+                EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY: delivery_time
+            }
+            current_chronology.insert(0, minute_info)  # Previous current minute becomes the second most recent minute in the chronology
+
+            # If chronology has unnecessary info, trim it
+            if len(current_chronology) > window_size:
+                current_chronology.pop(-1)
+
+        def on_event_for_new_non_consecutive_minute(current_chronology: list[dict[str, Union[datetime, int]]], next_minute: datetime, delivery_time: int, window: int, target_queue: queue.Queue):
+            # The current minute is now closed, with the arrival of a translation event that occurred in a minute after the next minute
+            # The info for the current minute should therefore be outputted
+            current_minute = current_chronology[0].get(EventProcessor.CHRONOLOGY_DATE_KEY)  # The current minute until the incoming event arrived
+            average_delivery_time: float = EventProcessor._get_average_delivery_time_from_chronology(current_chronology, window)
+            EventProcessor._notify_new_information(target_queue, datetime_object=current_minute, average_delivery_time=average_delivery_time)
+
+            # Handles the empty minutes between the minute of the current event and the minute of the incoming event
+            # Ex: If current event occurred at 12:34:56 and incoming event occurred at 12:36:56, minute 12:36:00 (12:35:00.000001 -> 12:36:00) is empty
+
+            minute_difference_timedelta: timedelta = next_minute - current_minute  # Ex: 12:37:00 - 12:35:00 = 00:02:00
+            minute_difference: int = minute_difference_timedelta.seconds // 60  # timedelta objects allow to get seconds, from which minutes can be deducted
+
+            for i in range(1, minute_difference):  # A difference of X minutes leads to X-1 empty minutes (in the example above, 2 minutes of difference leads to 1 empty minute)
+                # Adds a new empty minute in the chronology as the current minute
+                new_minute: datetime = current_minute + timedelta(minutes=i)  # In this for-loop, i starts at 1
+                minute_info: dict[str, Union[datetime, int]] = {
+                    EventProcessor.CHRONOLOGY_DATE_KEY: new_minute,
+                    EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY: 0,
+                    EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY: 0
+                }
+                current_chronology.insert(0, minute_info)  # Previous current minute becomes the second most recent minute in the chronology
+
+                # Notifies a new piece of information taking into account the new empty minute
+                average_delivery_time = EventProcessor._get_average_delivery_time_from_chronology(current_chronology, window)
+                EventProcessor._notify_new_information(target_queue, datetime_object=new_minute, average_delivery_time=average_delivery_time)
+
+                # If chronology has unnecessary info, trim it
+                if len(current_chronology) > window_size:
+                    current_chronology.pop(-1)
+
+            # At this point, all empty minutes have been added to the chronology (and maybe even removed), and the output has been updated accordingly
+            # The incoming translation event may now open a new minute in the chronology with an associated event
+            minute_info = {
+                EventProcessor.CHRONOLOGY_DATE_KEY: next_minute,
+                EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY: 1,
+                EventProcessor.CHRONOLOGY_TOTAL_DELIVERY_TIME_KEY: delivery_time
+            }
+            current_chronology.insert(0, minute_info)  # Previous current minute becomes the second most recent minute in the chronology
+
+            # If chronology has unnecessary info, trim it
+            if len(current_chronology) > window_size:
+                current_chronology.pop(-1)
 
         # Stores the information regarding the incoming translation events, from latest to earliest minute
         # The provided window size, in minutes, determines how long this chronology is. Ex: A window size of 10 will lead to a max size of 11 (eleven) elements in the list
@@ -117,10 +188,22 @@ class EventProcessor:
             #           - In this case, minute 12:34:00 has no associated events
 
             if not chronology:  # Event is the first in the input file
-                on_first_event(chronology, timestamp_next_minute, event_delivery_time)
+                on_first_event(chronology, timestamp_next_minute, event_delivery_time, outgoing_queue)
 
-        # All translation events have been read
-        EventProcessor._notify_end_of_information(outgoing_queue)
+            elif timestamp_next_minute == chronology[0].get(EventProcessor.CHRONOLOGY_DATE_KEY):  # Event belongs to the same minute as other previous event(s)
+                on_event_for_same_minute(chronology, event_delivery_time)
+
+            elif timestamp_next_minute - chronology[0].get(EventProcessor.CHRONOLOGY_DATE_KEY) == timedelta(minutes=1):  # Event is the first to be created in its minute AND previous event was created in the previous minute
+                on_event_for_new_consecutive_minute(chronology, timestamp_next_minute, event_delivery_time, window_size, outgoing_queue)
+
+            elif timestamp_next_minute - chronology[0].get(EventProcessor.CHRONOLOGY_DATE_KEY) > timedelta(minutes=1):  # Event is the first to be created in its minute AND previous event was created before the previous minute
+                on_event_for_new_non_consecutive_minute(chronology, timestamp_next_minute, event_delivery_time, window_size, outgoing_queue)
+
+        # All translation events have been read - Notify information for current minute (as it is now closed), and notify end of information to report
+        final_average_delivery_time: float = EventProcessor._get_average_delivery_time_from_chronology(chronology, window_size)
+        final_current_minute: datetime = chronology[0].get(EventProcessor.CHRONOLOGY_DATE_KEY)
+        EventProcessor._notify_new_information(outgoing_queue, datetime_object=final_current_minute, average_delivery_time=final_average_delivery_time)
+        EventProcessor._notify_end_of_information(outgoing_queue)  # Will cause Output Generator thread to exit on its own
         return
 
     @staticmethod
@@ -143,7 +226,8 @@ class EventProcessor:
         total_delivery_time: int = 0
 
         for i in range(window_size):
-            if i >= len(chronology) - 1:  # Oldest entry in chronology is always an "empty" minute, i.e. the full minute before the first translation event
+            # If the window size is larger than the chronology
+            if i >= len(chronology):
                 return _get_average_delivery_time(total_number_of_events, total_delivery_time)  # All information has been processed - Average delivery time can be computed and returned
 
             minute_number_of_events: int = chronology[i].get(EventProcessor.CHRONOLOGY_NUMBER_OF_EVENTS_KEY, 0)
@@ -151,6 +235,9 @@ class EventProcessor:
 
             total_number_of_events += minute_number_of_events
             total_delivery_time += minute_delivery_time
+
+        # If the chronology is larger than the window size
+        return _get_average_delivery_time(total_number_of_events, total_delivery_time)  # All information has been processed for the desired minutes - Average delivery time can be computed and returned
 
     @staticmethod
     def _get_parameters_from_raw_translation_event(raw_translation_event: str) -> tuple[datetime, int]:
